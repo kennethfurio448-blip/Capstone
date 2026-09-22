5d0549d16c87ea9371191100e9f7c2770b5856a2
kennethfurio448-blip <kennethfurio448@gmail.com>
2026-09-22 12:20:33 +0800

    Revert "Add password visibility toggle to MFA setup"
    This reverts commit 573906198a2e5a8c893972e17acfdb43041759a2.

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const CACHE_NAME = "medtrack-shell-v10-password-visibility";
+const CACHE_NAME = "medtrack-shell-v9-security-hardening";
 const APP_SHELL = [
     "/",
     "/index.html",
